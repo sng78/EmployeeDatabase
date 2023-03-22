641891de4d18407766d2c4364e18acaebe6fe391
Sergei Gorbachev <sng78@list.ru>
2023-03-22 23:32:38 +0300

    initial commit

---
+++ src/main/java/ru/sng78/spring/mvc_hibernate_aop/dao/EmployeeDAO.java
@@ -0,0 +1,15 @@
+package ru.sng78.spring.mvc_hibernate_aop.dao;
+
+import ru.sng78.spring.mvc_hibernate_aop.entity.Employee;
+
+import java.util.List;
+
+public interface EmployeeDAO {
+    public List<Employee> getAllEmployees();
+
+    public void saveEmployee(Employee employee);
+
+    public Employee getEmployee(int id);
+
+    public void deleteEmployee(int id);
+}
